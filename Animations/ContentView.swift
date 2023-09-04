e3b59d2b7e686cc5ea933578cecb25174871ad63
Marcio Curvello <marciocurvello@gmail.com>
2023-09-04 00:27:15 -0300

    Controlling the animation stack

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var enabled = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap me") {
+            enabled.toggle()
         }
-        .padding()
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .red)
+        .animation(nil, value: enabled)
+        .foregroundColor(.white)
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
     }
 }
 
